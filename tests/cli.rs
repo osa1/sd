098eb8c3d21b7c7c325b9b7b4a12e9b1efa6b549
Ömer Sinan Ağacan <omeragacan@gmail.com>
2023-08-12 20:59:46 +0200

    Remove anyhow in tests:
    You don't handle errors in tests so there's no need to propagate error
    values to callers.
    
    `unwrap` also fails as early as possible and with a proper stack trace.

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,9 +1,8 @@
 #[cfg(test)]
 #[cfg(not(sd_cross_compile))] // Cross-compilation does not allow to spawn threads but `command.assert()` would do.
 mod cli {
-    use anyhow::Result;
     use assert_cmd::Command;
-    use std::io::prelude::*;
+    use std::io::Write;
 
     fn sd() -> Command {
         Command::cargo_bin(env!("CARGO_PKG_NAME")).expect("Error invoking sd")
@@ -13,67 +12,63 @@ mod cli {
         assert_eq!(content, std::fs::read_to_string(path).unwrap());
     }
 
-    fn create_soft_link<P: AsRef<std::path::Path>>(src: &P, dst: &P) -> Result<()> {
+    fn create_soft_link<P: AsRef<std::path::Path>>(src: &P, dst: &P) {
         #[cfg(target_family = "unix")]
-        std::os::unix::fs::symlink(src, dst)?;
-        #[cfg(target_family = "windows")]
-        std::os::windows::fs::symlink_file(src, dst)?;
+        std::os::unix::fs::symlink(src, dst).unwrap();
 
-        Ok(())
+        #[cfg(target_family = "windows")]
+        std::os::windows::fs::symlink_file(src, dst).unwrap();
     }
 
     #[test]
-    fn in_place() -> Result<()> {
-        let mut file = tempfile::NamedTempFile::new()?;
-        file.write_all(b"abc123def")?;
+    fn in_place() {
+        let mut file = tempfile::NamedTempFile::new().unwrap();
+        file.write_all(b"abc123def").unwrap();
         let path = file.into_temp_path();
 
         sd().args(["abc\\d+", "", path.to_str().unwrap()])
             .assert()
             .success();
         assert_file(&path, "def");
-
-        Ok(())
     }
 
     #[test]
-    fn in_place_with_empty_result_file() -> Result<()> {
-        let mut file = tempfile::NamedTempFile::new()?;
-        file.write_all(b"a7c")?;
+    fn in_place_with_empty_result_file() {
+        let mut file = tempfile::NamedTempFile::new().unwrap();
+        file.write_all(b"a7c").unwrap();
         let path = file.into_temp_path();
 
         sd().args(["a\\dc", "", path.to_str().unwrap()])
             .assert()
             .success();
         assert_file(&path, "");
-
-        Ok(())
     }
 
     #[test]
-    fn in_place_following_symlink() -> Result<()> {
-        let dir = tempfile::tempdir()?;
+    fn in_place_following_symlink() {
+        let dir = tempfile::tempdir().unwrap();
         let path = dir.path();
         let file = path.join("file");
         let link = path.join("link");
 
-        create_soft_link(&file, &link)?;
-        std::fs::write(&file, "abc123def")?;
+        create_soft_link(&file, &link);
+        std::fs::write(&file, "abc123def").unwrap();
 
         sd().args(["abc\\d+", "", link.to_str().unwrap()])
             .assert()
             .success();
 
         assert_file(&file, "def");
-        assert!(std::fs::symlink_metadata(link)?.file_type().is_symlink());
-
-        Ok(())
+        assert!(std::fs::symlink_metadata(link)
+            .unwrap()
+            .file_type()
+            .is_symlink());
     }
 
     #[test]
-    fn replace_into_stdout() -> Result<()> {
-        let mut file = tempfile::NamedTempFile::new()?;
-        file.write_all(b"abc123def")?;
+    fn replace_into_stdout() {
+        let mut file = tempfile::NamedTempFile::new().unwrap();
+        file.write_all(b"abc123def").unwrap();
 
         sd().args(["-p", "abc\\d+", "", file.path().to_str().unwrap()])
             .assert()
@@ -85,18 +80,14 @@ mod cli {
             ));
 
         assert_file(file.path(), "abc123def");
-
-        Ok(())
     }
 
     #[test]
-    fn stdin() -> Result<()> {
+    fn stdin() {
         sd().args(["abc\\d+", ""])
             .write_stdin("abc123def")
             .assert()
             .success()
             .stdout("def");
-
-        Ok(())
     }
 }
